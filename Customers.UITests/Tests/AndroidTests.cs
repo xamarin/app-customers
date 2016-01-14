c6225bd60cedcc4a6c7ab3f48e3cc1acd95ca13f
Ethan Dennis <erdennis13@gmail.com>
2016-01-14 09:38:02 -0800

    Added initial UITest project structure

---
+++ Customers.UITests/Tests/AndroidTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Customers.UITests
+{
+    [TestFixture(Platform.Android)]
+    public class AndroidTests
+    {
+        IApp app;
+        Platform platform;
+
+        public AndroidTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+    }
+}
+
---
+++ Customers.UITests/Tests/iOSTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Customers.UITests
+{
+    [TestFixture(Platform.iOS)]
+    public class iOSTests
+    {
+        IApp app;
+        Platform platform;
+
+        public iOSTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+    }
+}
+

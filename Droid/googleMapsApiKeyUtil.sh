14fcc46535f3705c9bd4e35a0b3d0fca4b23ab0f
Joe Sauve <joe.sauve@xamarin.com>
2016-01-19 13:29:54 -0600

    Added bash script for injecting Google Maps API key into Android bundle. Mostlyyl for automated build processes.
    Usage:
    sh googleMapsApiKeyUtil.sh -m Properties/AndroidManifest.xml -p com.google.android.geo.API_KEY -g [you actual Google Maps API key]

---
+++ Droid/googleMapsApiKeyUtil.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+
+manifestPath=
+placeholderKey=
+googleMapsKey=
+
+while getopts "m:p:g:" o; do
+	case "${o}" in
+		m) manifestPath="${OPTARG}" ;;
+		p) placeholderKey="${OPTARG}" ;;
+		g) googleMapsKey="${OPTARG}" ;;
+	esac
+done
+shift $((OPTIND-1))
+
+sed -i '' "s/$placeholderKey/$googleMapsKey/g" "$manifestPath"
